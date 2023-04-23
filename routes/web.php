e89971c773975ea50a7e5bc84d862f4f429015fe
Sachiya999 <spsd49@gmail.com>
2023-04-23 17:29:04 +0530

    view posts and zero post view posts index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function(){
+//     return view('Welcome');
+// });
+
+// Route::get('/', function () {
+//     return '<h1>Hello World</h1>';
+// });
+
+//home index view
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
+})->name('home.index');
+
+
+//about index view
+Route::get('/about', function(){
+    return '<h1>About</h1>';
+});
+
+//index view
+Route::get('/posts', function(){
+    $posts = [
+        ['id' => 1, 'title' => 'post 1', 'content' => 'content 1'],
+        ['id' => 2, 'title' => 'post 2', 'content' => 'content 2'],
+        ['id' => 4, 'title' => 'post 4', 'content' => 'content 4'],
+        ['id' => 5, 'title' => 'post 5', 'content' => 'content 5'],
+        ['id' => 6, 'title' => 'post 6', 'content' => 'content 6'],
+    ];
+    return view('posts.index', ['posts' => $posts]);
+})->name('posts.index');
+
+//single post view
+Route::get('/post/{id?}', function($id = 0){
+    if($id == 0)
+    {
+        return redirect()->route('posts.index');
+    }
+    return '<h1>Post '.$id. '</h1>';
+})->name('posts.show');
+
+
+//single post view edit
+Route::get('/post/{id}/edit', function($id){
+    return '<h1>Post '.$id. ' Edit</h1>';
 });
