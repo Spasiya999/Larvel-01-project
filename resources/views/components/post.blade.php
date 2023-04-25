f42af5b3dcd16e1cebd746320e3feee6db7b74cb
Sachintha Pasindu <spsd49@gmail.com>
2023-04-26 00:05:33 +0530

    delete

--- resources/views/components/post.blade.php
+++
@@ -1,8 +0,0 @@
-<div @style([
-    'background-color:gray' => $loop->odd,
-])>
-    <h3>{{ $post['id'] }}. {{ $post['title'] }}</h3>
-    <p>{{ $post['content'] }}</p>
-    <a href="{{ route('posts.show', $post['id']) }}">Read more</a>
-    <hr>
-</div>
